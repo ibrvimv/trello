c7ca7cd06c4c6f3ad5d01ce0695f0b972a5d4297
ibrvimv <ibrvimv@gmail.com>
2024-01-20 19:29:25 +0600

    edit btn

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,5 @@
 import { DraggableProvided } from 'react-beautiful-dnd';
-
+import EditIcon from '@mui/icons-material/Edit';
 type CardType = {
   bgColor: string;
   title: string;
@@ -21,7 +21,13 @@ export default function Card({
       {...provided.draggableProps}
       ref={provided.innerRef}
     >
-      <h3>{props.title}</h3>
+      <div className='flex justify-between items-center'>
+        <h3>{props.title}</h3>
+        <div className='p-2 cursor-pointer hover:opacity-30 transition-opacity duration-150'>
+          <EditIcon fontSize='medium' />
+        </div>
+      </div>
+
       <h4>{props.subtitle}</h4>
       <p>{props.text}</p>
     </div>
